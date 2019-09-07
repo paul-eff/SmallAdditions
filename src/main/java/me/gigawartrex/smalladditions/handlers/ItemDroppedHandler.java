fb82c0ee0c6594c916132a94d7236244167e9b9b
Paul Ferlitz <paul.ferlitz@hotmail.com>
2019-09-07 16:42:51 +0200

    Starting rotten flesh to meat

--- src/main/java/me/gigawartrex/smalladditions/handlers/ItemDroppedHandler.java
+++ src/main/java/me/gigawartrex/smalladditions/handlers/ItemDroppedHandler.java
@@ -1,21 +1,16 @@
 package me.gigawartrex.smalladditions.handlers;
 
 import me.gigawartrex.smalladditions.files.Config;
-import me.gigawartrex.smalladditions.helpers.Helper;
-import me.gigawartrex.smalladditions.helpers.Leveling;
 import me.gigawartrex.smalladditions.helpers.MessageHelper;
 import me.gigawartrex.smalladditions.main.Constants;
-import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
-import org.bukkit.block.BlockFace;
-import org.bukkit.entity.ExperienceOrb;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -23,7 +18,8 @@ import org.bukkit.scheduler.BukkitRunnable;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class ItemDroppedHandler implements Listener {
+public class ItemDroppedHandler implements Listener
+{
 
     private Config config;
     private MessageHelper msghelp;
@@ -39,43 +35,66 @@ public class ItemDroppedHandler implements Listener {
      * Method to handle tree felling onBlockBreak event
      */
     @EventHandler
-    public void onItemDropped(PlayerDropItemEvent event) {
+    public void onItemDropped(PlayerDropItemEvent event)
+    {
 
         //Load needed classes
         config = new Config();
         msghelp = new MessageHelper();
         eventPlayer = event.getPlayer();
 
-        //if (event.getPlayer().isSneaking()) {
-            //Check if player is dropping valid item
-            if (allowedItems.contains(event.getItemDrop().getItemStack().getType())) {
+        if (allowedItems.contains(event.getItemDrop().getItemStack().getType()))
+        {
+            Item drop = event.getItemDrop();
+            System.out.println(drop.getItemStack().getType() + " was dropped");
 
-                Item drop = event.getItemDrop();
-                msghelp.sendConsole(event.getItemDrop().getItemStack().getType().toString()+" dropped!", ChatColor.BLUE);
-
-                new BukkitRunnable() {
-                    @Override
-                    public void run() {
-                        if (drop.isOnGround()) {
-                            eventBlockLocation = event.getItemDrop().getLocation().getBlock().getRelative(BlockFace.UP);
-                            System.out.println("????");
-                            this.cancel();
+            new BukkitRunnable()
+            {
+                @Override
+                public void run()
+                {
+                    if (drop.isOnGround())
+                    {
+                        //msghelp.sendPlayer(eventPlayer, drop.getName() + " is on ground at " + drop.getLocation().toString(), ChatColor.RED);
+                        boolean foundFlesh = false;
+                        boolean foundStone = false;
+                        int fleshIndex = 0;
+                        int stoneIndex = 0;
+                        ArrayList<Entity> nearbyEntities = (ArrayList<Entity>) drop.getNearbyEntities(1, 1, 1);
+                        String outString = "";
+                        for (Entity ent : nearbyEntities)
+                        {
+                            outString += ent.getType() + ", ";
                         }
-
-                    }
-                }.runTaskTimer(Constants.plugin, 0, 1L);
-
-                System.out.println(eventBlockLocation.getX());
-
-                /*if(eventBlockLocation.getDrops().size() >= 2){
-                    if(eventBlockLocation.getDrops().contains(Material.STONE) && eventBlockLocation.getDrops().contains(Material.ROTTEN_FLESH)) {
-                        for (ItemStack item : eventBlockLocation.getDrops()) {
-                            msghelp.sendConsole("FOUND: "+item.getType().toString(), ChatColor.BLUE);
+                        System.out.println("On ground " + outString);
+                        for (Entity nearDrop : nearbyEntities)
+                        {
+                            if (nearDrop.getType().equals(Material.ROTTEN_FLESH))
+                            {
+                                System.out.println("found flesh");
+                                foundFlesh = true;
+                                fleshIndex = nearbyEntities.indexOf(nearDrop);
+                            } else if (nearDrop.getType().equals(Material.STONE))
+                            {
+                                System.out.println("found stone");
+                                foundStone = true;
+                                stoneIndex = nearbyEntities.indexOf(nearDrop);
+                            }
+                            if (foundFlesh && foundStone)
+                            {
+                                drop.getWorld().dropItemNaturally(drop.getLocation(), new ItemStack(Material.PORKCHOP));
+                            }
                         }
+                        if (foundFlesh && foundStone)
+                        {
+                            nearbyEntities.get(fleshIndex).remove();
+                            nearbyEntities.get(stoneIndex).remove();
+                        }
+                        this.cancel();
                     }
-                }*/
-            }
-        //}
+                }
+            }.runTaskTimer(Constants.plugin, 20L, 20L);
+        }
     }
 
     /**
@@ -84,15 +103,27 @@ public class ItemDroppedHandler implements Listener {
      * @param player The player which's item should be damaged
      * @param item   The item which should be damaged
      */
-    private void damageItem(Player player, ItemStack item) {
-
+    private void damageItem(Player player, ItemStack item)
+    {
         item.setDurability((short) (item.getDurability() + 1));
-        if (item.getDurability() >= item.getType().getMaxDurability()) {
+        if (item.getDurability() >= item.getType().getMaxDurability())
+        {
 
             player.getInventory().removeItem(item);
-        } else {
+        } else
+        {
 
             player.getInventory().setItemInMainHand(item);
         }
+    }
+
+    private ArrayList<Item> getNearbyDroppedItems(Item item)
+    {
+        ArrayList<Item> nearbyDrops = new ArrayList<>();
+        nearbyDrops.add(item);
+        Block itemLoc = item.getLocation().getBlock();
+
+
+        return nearbyDrops;
     }
 }
